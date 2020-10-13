9eddb61451b230df444185a58efc19a2cc31afe0
desmond93 <bondarenkomaxim93@gmail.com>
2020-10-13 12:40:33 +0300

    Updates

---
+++ precache-manifest.4cfebb3591c0d8a3623518706b54cc98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16c22ab35728243f7a27c4ac99fad47",
+    "url": "/weatherApp_React/index.html"
+  },
+  {
+    "revision": "8758fafdd4b062377ff6",
+    "url": "/weatherApp_React/static/css/main.f8f29202.chunk.css"
+  },
+  {
+    "revision": "bc4dd60363cbfb2da254",
+    "url": "/weatherApp_React/static/js/2.ea00a3d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherApp_React/static/js/2.ea00a3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8758fafdd4b062377ff6",
+    "url": "/weatherApp_React/static/js/main.64ea6d04.chunk.js"
+  },
+  {
+    "revision": "a6d16d2820219f9709e4",
+    "url": "/weatherApp_React/static/js/runtime-main.0e9c4494.js"
+  }
+]);
